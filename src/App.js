a5b963e0e1ae77ceeca71118f0bb248601e9f52d
Erik Dymbe <erik.dymbe@gmail.com>
2018-04-14 10:19:33 +0200

    Make the tabs displays the ObsTab-component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import './App.css';
 import ROSLIB from 'roslib';
 import RosError from './components/RosError';
-import {Tab} from 'semantic-ui-react'
-import General from './components/generalTab/GeneralTab'
+import {Tab} from 'semantic-ui-react';
+import GeneralTab from './components/generalTab/GeneralTab';
+import ObsTab from './components/obsTab/ObsTab';
 
 const panes = [
-    {menuItem: 'General', render: () => <Tab.Pane><General ros={this.ros}/></Tab.Pane>},
-    {menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane>},
+    {menuItem: 'General', render: () => <Tab.Pane><GeneralTab/></Tab.Pane>},
+    {menuItem: 'OBS', render: () => <Tab.Pane><ObsTab/></Tab.Pane>},
     {menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane>},
 ];
 
@@ -21,38 +22,18 @@ class App extends Component {
         this.ros = new ROSLIB.Ros({
             url: 'ws://localhost:9090'
         });
-
-        let listener = new ROSLIB.Topic({
-            ros: this.ros,
-            name: '/test',
-            messageType: 'std_msgs/String'
-        });
-
-        listener.subscribe((message) => {
-            this.setState({
-                "text": message.data,
-            });
-            console.log('Received message on ' + listener.name + ': ' + message.data);
-        });
     }
 
     componentWillMount() {
-        /*
         this.ros.on('connection', () => {
             console.log('Connected to websocket server.');
             this.setState({"rosIsConnected": true});
-        });
-
-        this.ros.on('error', (error) => {
-            console.log('Error connecting to websocket server: ', error);
         });
 
         this.ros.on('close', () => {
             console.log('Connection to websocket server closed.');
             this.setState({"rosIsConnected": false});
         });
-
-        */
     }
 
     render() {
