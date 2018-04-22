aedd34cf08512e36a8a8bbc28441b650242b8275
Erik Dymbe <erik.dymbe@gmail.com>
2018-04-22 15:45:49 +0200

    Create DataTable component
    For displaying data

---
+++ src/components/obsTab/DataTable.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './DataTable.css'
+
+const DataTable = (props) => (
+    <table className="data-table">
+        {props.data.map(data => (
+            <tr key={data.x}>
+                <td>{data.x}</td>
+                <td>{data.y.toFixed(1)}</td>
+            </tr>
+        ))}
+    </table>
+);
+
+export default DataTable;
