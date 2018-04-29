a8c8d39b4ab511e08554fbdd7641549ab9ecf57e
Erik Dymbe <erik.dymbe@gmail.com>
2018-04-29 16:58:52 +0200

    Create rosbridge-server subprocess on startup
    This will not work if you do not have rosbridge_server installed.
    Also, as of now the rosbridge server has a longer startup-time
    than the application, therefore the error message will show for
    a brief second. This should be fixed, for example by creating the
    application window when the rosbridge-server is ready.

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -7,6 +7,22 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
+// Starts the rosbridge-server automatically
+const {spawn} = require('child_process');
+const rosbridge = spawn('roslaunch', ['rosbridge_server', 'rosbridge_websocket.launch']);
+
+rosbridge.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+rosbridge.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+});
+
+rosbridge.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
