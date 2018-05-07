91305f8c2339324be1e35b43fd839ca55b93604f
Erik Dymbe <erik.dymbe@gmail.com>
2018-05-07 22:34:06 +0200

    Create a tab for liftbag-release
    Maybe the liftbag doesnt really need it's own tab, but it
    was natural at the moment.

---
+++ src/components/liftbagTab/LiftbagTab.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './LiftbagTab.css';
+import BigRedButton from '../common/BigRedButton/BigRedButton';
+
+function LiftbagTab() {
+    return <div id="liftbag-tab"><BigRedButton text="Release"/></div>
+}
+
+export default LiftbagTab;
