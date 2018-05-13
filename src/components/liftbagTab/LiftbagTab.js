abd858e32ac9d555e934be9ac671112c7939daa8
Erik Dymbe <erik.dymbe@gmail.com>
2018-05-13 09:42:45 +0200

    Publish message on button press

--- src/components/liftbagTab/LiftbagTab.js
+++ src/components/liftbagTab/LiftbagTab.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './LiftbagTab.css';
 import BigRedButton from '../common/BigRedButton/BigRedButton';
+import ROSLIB from 'roslib';
 
-function LiftbagTab() {
-    return <div id="liftbag-tab"><BigRedButton text="Release"/></div>
+class LiftbagTab extends Component {
+    componentDidMount() {
+        const ros = new ROSLIB.Ros({url: 'ws://localhost:9090'});
+        this.topic = new ROSLIB.Topic({
+            ros: ros,
+            name: '/liftbag_release',
+            messageType: 'std_msgs/String',
+        });
+    }
+
+    componentWillUnmount() {
+        this.topic.unsubscribe();
+    }
+
+    render() {
+        return <div id="liftbag-tab"><BigRedButton onClick={() => this.topic.publish({data: 'Release'})} text="Release"/></div>
+    }
 }
 
 export default LiftbagTab;
