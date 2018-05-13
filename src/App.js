092a45e7fc1400e24b5f60ab3fcf5bc6f11da6b9
Erik Dymbe <erik.dymbe@gmail.com>
2018-05-13 18:53:46 +0200

    Added the search-zone tab to the GUI

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import {Tab} from 'semantic-ui-react';
 import GeneralTab from './components/generalTab/GeneralTab';
 import ObsTab from './components/obsTab/ObsTab';
 import AircraftIdTab from './components/aircraftIdTab/AircraftIdTab';
+import SearchZoneTab from './components/search-zone-tab/SearchZoneTab';
 
 const panes = [
     {menuItem: 'General', render: () => <Tab.Pane><GeneralTab/></Tab.Pane>},
     {menuItem: 'OBS', render: () => <Tab.Pane><ObsTab/></Tab.Pane>},
     {menuItem: 'Aircraft identification', render: () => <Tab.Pane><AircraftIdTab/></Tab.Pane>},
+    {menuItem: 'Search zone', render: () => <Tab.Pane><SearchZoneTab/></Tab.Pane>},
 ];
 
 class App extends Component {
