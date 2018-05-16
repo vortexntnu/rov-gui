f08e1cdf7044e5e815dbc77f43a8369823e4bd92
Erik Dymbe <erik.dymbe@gmail.com>
2018-05-16 09:57:14 +0200

    Add a component to display search-zone-output

---
+++ src/components/search-zone-tab/SearchZoneOutput.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class SearchZoneOutput extends Component {
+    render() {
+        console.log("halla");
+        return <div>hei</div>
+    }
+}
+
+export default SearchZoneOutput;
