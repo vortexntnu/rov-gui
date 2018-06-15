e3e794183795420b176b42badfee7546ffb24357
Erik Dymbe <erik.dymbe@gmail.com>
2018-06-15 20:26:42 +0200

    Make ROV-monitor-tab semi-functional
    Needs to be integrated with the ROV

--- src/components/rov-monitor/Device.js
+++ src/components/rov-monitor/Device.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import './Device.css'
+import './Device.css';
+import {Card} from 'semantic-ui-react';
 
-const Device = (props) => {
+/*
+
+# Serves as heartbeat and continuous device monitoring
+
+Header header # Primarly for timestamp
+string id # Device serial number
+string ip # Ethernet IP adress
+string type # Device type, eg. camera, manipulator, etc
+int8 leak # 0 for no leak, 1 for leak, -1 for data unavailable
+int8 cpu_temp # -1 for data unavailable
+
+*/
+
+function leakStatus(leak) {
+    if(leak === 1) {
+        return <div className='leak'>LEAK!</div>;
+    } else if(leak === 0) {
+        return <div className='no-leak'>None</div>;
+    } else {
+        return <div className='leak-unknown'>Unknown</div>;
+    }
+}
+
+const Device = ({device}) => {
+    const {ip, type, leak, cpu_temp} = device;
     return (
-        <div>This is a device</div>
+        <Card>
+            <Card.Content>
+                <Card.Header>{ip}</Card.Header>
+                <div className='row'>
+                    <div>Type:</div>
+                    <div>{type}</div>
+                </div>
+                <div className='row'>
+                    <div>CPU-temperature:</div>
+                    <div>{cpu_temp}</div>
+                </div>
+                <div className='row'>
+                    <div>Leak</div>
+                    {leakStatus(leak)}
+                </div>
+            </Card.Content>
+        </Card>
     )
 };
 
--- src/components/rov-monitor/RovMonitorTab.js
+++ src/components/rov-monitor/RovMonitorTab.js
@@ -2,6 +2,80 @@ import React, {Component} from 'react';
 import './RovMonitorTab.css'
 import {Tab} from 'semantic-ui-react';
 import ROSLIB from 'roslib';
+import Device from './Device';
+
+const testList = [
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+    {
+        header: '',
+        ip: '192.168.819.230',
+        type: 'Camera',
+        leak: 0,
+        cpu_temp: 65,
+    },
+];
 
 class RovMonitorTab extends Component {
     constructor(props) {
@@ -10,8 +84,8 @@ class RovMonitorTab extends Component {
         const ros = new ROSLIB.Ros({url: process.env.REACT_APP_ROSBRIDGE_URL});
         this.topic = new ROSLIB.Topic({
             ros: ros,
-            name: '',
-            messageType: ''
+            name: '/device_list',
+            messageType: 'DeviceList'
         });
         this.topic.subscribe(this.handleRos);
     }
@@ -22,8 +96,14 @@ class RovMonitorTab extends Component {
     };
 
     render() {
+        const {devices} = this.state;
         return (
-            <Tab.Pane id='rov-monitor'>RovMonitorTab works!</Tab.Pane>
+            <Tab.Pane id='rov-monitor'>
+                <div className='device-grid'>
+                    {devices.map((device, i) => <Device key={i} device={device}/>)}
+                    {[1,1,1,1,1,1,1,1,1,1].map((d, i) => <div key={i} className='filler-card'/>)}
+                </div>
+            </Tab.Pane>
         );
     }
 }
